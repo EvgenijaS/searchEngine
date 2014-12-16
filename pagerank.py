c7c71a2bdba5c7170908ca662813a995fadbc4ae
Verica Lazova <vericalcroft@gmail.com>
2014-12-16 18:27:43 +0100

    Power method implementation added

--- pagerank.py
+++ pagerank.py
@@ -1,16 +1,8 @@
 import numpy as np
 
 """
-1-funkcija koja od dictionary gradi 1-0 matrica
-DONE
-2-funkcija koja od adjacency matrix gradi transition matrix
-DONE
-4-funkcija koja ja inicijalizira raspredelbata
-3-funkcija koja implementira power method
 4-funkcija koja go sortira pagerankot
 """
-
-
 
 def adjacency_matrix(links):
     """
@@ -40,11 +32,11 @@ def transition_matrix(A, df):
         A is the adjacency matrix and df is the damping factor
     """
     nodes = A.shape[0]
-    Q = np.empty(shape = adj.shape)
+    Q = np.empty(shape = A.shape)
     Q.fill(df/nodes)
-    row_sums = np.apply_along_axis(sum, 1, adj)
-    Q = Q + np.apply_along_axis(normalize, 0, adj, row_sums, df)
-    return tm
+    row_sums = np.apply_along_axis(sum, 1, A)
+    Q = Q + np.apply_along_axis(normalize, 0, A, row_sums, df)
+    return Q
 
 def normalize(x, sums, df):
     tmp = np.divide(x, sums)
@@ -56,7 +48,20 @@ def power_method(Q):
     """
        Implements the power method for computing page rank with restarts
     """
-    # TODO
+    nodes = Q.shape[0]
+    p = np.empty((nodes, 1))
+    p.fill(1.0/nodes)
+    Q = np.transpose(Q)
+
+    old_p = np.zeros(p.shape)
+    while np.sum(np.absolute(old_p - p)) > 10e-5:
+        print np.sum(np.absolute(old_p - p))
+        old_p = np.copy(p)
+        p = np.dot(Q, p)
+
+    return p
+
+#------------------------------------------------------------------------------
 
 ########################## TEST ##############################
 def main ():
@@ -71,9 +76,11 @@ def main ():
 
     idmap, A = adjacency_matrix(d)
     Q = transition_matrix(A, 0.2);
+    p = power_method(Q)
 
-    print Q
+    print idmap
     print
-    print np.apply_along_axis(sum, 1, Q)
+    print p
+
 
 main()
